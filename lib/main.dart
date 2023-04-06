b19ba090a80e1a249e7d80cbc8a34f16b111a786
Alexander Troshkov <20989940+aednlaxer@users.noreply.github.com>
2023-04-06 09:59:55 +0300

    Add semantics debugger switch

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,38 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool _showSemanticsDebugger = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      showSemanticsDebugger: _showSemanticsDebugger,
       title: 'Accessibility issues',
-      home: const HomePage(),
+      home: Column(
+        children: [
+          Material(
+            child: SwitchListTile(
+              controlAffinity: ListTileControlAffinity.leading,
+              onChanged: (value) {
+                setState(() => _showSemanticsDebugger = value);
+              },
+              value: _showSemanticsDebugger,
+              title: const Text('Toggle semantics debugger overlay'),
+            ),
+          ),
+          const Expanded(
+            child: HomePage(),
+          ),
+        ],
+      ),
       builder: (_, child) => AccessibilityTools(child: child),
     );
   }
